77bff35ce4b6b1f661952cd7c291848ba35cd3b4
Matheus <matheusvrgo2@gmail.com>
2024-12-29 23:02:21 -0300

    Adicionado sistema de auth

--- Frontend/src/pages/auth/page.jsx
+++ Frontend/src/pages/auth/page.jsx
@@ -1,34 +1,69 @@
 import styles from './page.module.css'
 import { useState } from "react"
 import { TextField, Button } from "@mui/material"
+import authServices from '../../services/auth.jsx'
 
 export default function Auth() {
     const [ formType, setFormType ] = useState('login')
+    const [ formData, setFormData ] = useState(null)
+    const { login, signup, authLoading } = authServices()
     
     const handleChangeFormType = () => {
+        setFormData(null)
         if(formType ==='login') {
             setFormType('signup')
         } else {
             setFormType('login')
+        } 
+    }
+
+    const handleFormDataChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault()
+        switch (formType) {
+            case 'login':
+                login(formData)
+                break;
+            case 'signup':
+                if(formData.password !== formData.confirmPassword) {
+                    console.log('Passwords do not match')
+                    return
+                }
+                signup(formData)
+
+                break;
         }
     }
+
+    if(authLoading) {
+        return ( <h1>Loading</h1>)
+    }
+
     if(formType === 'login') {
         return (
             <div className={styles.allPage}>
                 <div className={styles.authContainer}>
                     <h1>Login</h1>
-                    <form className={styles.formContainer}>
+                    <form className={styles.formContainer} onSubmit={handleSubmitForm}>
                         <TextField className={styles.emailBox}
                         required
                         label="Email"
                         type="email"
                         name="email"
+                        onChange={handleFormDataChange}
                         />
                         <TextField className={styles.passBox}
                         required
                         label="Password"
                         type="password"
                         name="password"
+                        onChange={handleFormDataChange}
                         />
                         <Button variant="contained"  type="submit">Login</Button>
                     </form>
@@ -38,29 +73,43 @@ export default function Auth() {
         )
     }
     
-    const handleFormDataChange = (e) => {
-        
-    }
+    
 
     if(formType === 'signup') {
         return (
             <div className={styles.allPage}>
                 <div className={styles.authContainer}>
                     <h1>SIGN UP</h1>
-                    <form className={styles.formContainer}>
+                    <form className={styles.formContainer} onSubmit={handleSubmitForm}>
+                        <TextField className={styles.emailBox}
+                        required
+                        label="Fullname"
+                        type="fullname"
+                        name="fullname"
+                        onChange={handleFormDataChange}
+                        />
                         <TextField className={styles.emailBox}
                         required
                         label="Email"
                         type="email"
                         name="email"
+                        onChange={handleFormDataChange}
                         />
                         <TextField className={styles.passBox}
                         required
                         label="Password"
                         type="password"
                         name="password"
+                        onChange={handleFormDataChange}
                         />
-                        <Button variant="contained"  type="submit">Login</Button>
+                        <TextField className={styles.passBox}
+                        required
+                        label="Confirm Password"
+                        type="password"
+                        name="confirmPassword"
+                        onChange={handleFormDataChange}
+                        />
+                        <Button variant="contained"  type="submit">Sign Up</Button>
                     </form>
                     <a className={styles.btnSwitch} onClick={handleChangeFormType}>Already have an account? Login</a>
                 </div>
