08187bb64631cd406567b6ebc95516bd72acd4a1
Matheus <matheusvrgo2@gmail.com>
2025-01-03 22:38:59 -0300

    Finalizado projeto

--- Frontend/src/services/auth.jsx
+++ Frontend/src/services/auth.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 export default function authServices() {
     const [authLoading, setAuthLoading] = useState(false)
 
-    const url = 'http://localhost:3000/auth'
+    const url = 'https://projeto-gastronomy-backend.onrender.com/auth'
 
     const login = (formData) => {
         setAuthLoading(true)
--- Frontend/src/services/plates.jsx
+++ Frontend/src/services/plates.jsx
@@ -5,7 +5,7 @@ export default function platesServices() {
     const [refetchPlates, setRefetchPlates] = useState(true)
     const [platesList, setPlatesList] = useState([])
 
-    const url = 'http://localhost:3000/plates'
+    const url = 'https://projeto-gastronomy-backend.onrender.com/plates'
 
     const getAvailablePlates = () => {
         setPlatesLoading(true)
